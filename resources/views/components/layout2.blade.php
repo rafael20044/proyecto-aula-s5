d089359a478e235b1a0ae97ad6ded536a5201c1c
Maria alejandra perez hernandez <mariagale2468@gmail.com>
2025-05-05 20:07:17 -0500

    inicio del login

--- resources/views/components/layout2.blade.php
+++ resources/views/components/layout2.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $titulo }}</title>
+    @vite(['resource/css/layout2.css'])
 </head>
 <body>
     {{ $slot }}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,3 @@
 @component('components.layout2', ['titulo' => 'iniciar session'])
-    <h1>PHP</h1>
+
 @endcomponent
