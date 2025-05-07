79e892f8c41412151544f47473f4fc1f28ca8cb5
Maria alejandra perez hernandez <mariagale2468@gmail.com>
2025-05-06 19:49:10 -0500

    login casi terminado,falta centrar info

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,3 +1,37 @@
-@component('components.layout2', ['titulo' => 'iniciar session'])
+@component('components.layout2', ['titulo' => 'iniciar session']) 
 
+<body style="background-image: url('{{ asset('img/login1.avif') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; background-attachment: fixed; height: 100vh; margin: 0;">
+
+    <div class="box">
+        <div class="wrapper">
+        <div class="container">
+            <h2>LOGIN</h2>
+
+            <div class="input-field">
+                <input type="text" class="input" placeholder="Usuario" required>
+            </div><br>
+
+            <div class="input-field">
+                <input type="password" class="input" placeholder="Contraseña" required>
+              
+            </div><br>
+
+            <div class="input-field">
+                <input type="submit" class="submit" value="Inicio">
+            </div><br>
+
+            <div class="bottom">
+                <div class="left">
+                    <input type="checkbox" id="check">
+                    <label for="check">Recordarme</label>
+            </div><br>
+
+            <div class="right">
+                <label><a href="#">¿Olvidaste la contraseña?</a></label>
+            </div><br>
+        </div>
+    </div>
+</div>
+ </div>
+</body>
 @endcomponent
